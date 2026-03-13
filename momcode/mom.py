384ae5d9ebe255f2e25fe4e8c3a7fa942e38455a
Ian <ian@DESKTOP-JNQ9OEJ.localdomain>
2026-03-13 20:46:07 +0800

    Monthsary

---
+++ momcode/mom.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(page_title="Monthsary Message ❤️", page_icon="❤️")
+
+st.title("💖 Happy 31st Monthsary Mommmmyyy koooooooo!!!! 💖")
+
+st.write("I LOVEEEEEE YOUUUUUU BABYYYYYYYYY ❤️")
+
+st.balloons()
+
+st.markdown("""
+### Dear Mommmmyyy,
+
+Happy **31st Monthsary**! 🥰  
+I loveeeeeeeeeee youuuuuuuuuuuu so much alwaysss mommmyyyyyy babyyyyy langlangggggg
+
+**-dadyyyyyy-**
+""")
+
+st.divider()
+
+if st.button("Click Here 🎁"):
+    st.success("I loveeeeeeeeeeeeeeeee youuuuuuuuuuuuuu mommyyyyy langlangggggg! 💕")
+    st.balloons()
+
+st.caption("Made with ❤️titi")
